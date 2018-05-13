c1b645e80396bbcb908f448254dd77589380017d
revel-media <ahmedfci37@yahoo.com>
2018-05-13 13:58:38 +0200

    initial commit

--- app/src/main/java/app/reze1/ahmed/reze1/fragments/Home.java
+++ app/src/main/java/app/reze1/ahmed/reze1/fragments/Home.java
@@ -83,6 +83,7 @@ import static android.content.Context.MODE_PRIVATE;
  * create an instance of this fragment.
  */
 public class Home extends Fragment {
+    //todo
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
