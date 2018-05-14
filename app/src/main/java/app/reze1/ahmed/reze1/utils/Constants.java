821e630dca36729ab4a1e2aa92148e563b058f73
revel-media <ahmedfci37@yahoo.com>
2018-05-14 14:46:21 +0200

    ui

--- app/src/main/java/app/reze1/ahmed/reze1/utils/Constants.java
+++ app/src/main/java/app/reze1/ahmed/reze1/utils/Constants.java
@@ -10,5 +10,5 @@ public class Constants {
     public static final String ARG_FIREBASE_TOKEN = "firebaseToken";
     public static final String ARG_FRIENDS = "friends";
     public static final String ARG_UID = "uid";
-    //todo
+    //to
 }
